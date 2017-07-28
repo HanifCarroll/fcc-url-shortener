fedc8ddeec1467774f0048f631aab9a31359879e
Hanif Carroll <HCarrollUSF@gmail.com>
2017-07-27 21:17:57 -0400

    Test

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const validUrl = require('valid-url');
+const shortid = require('shortid');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -11,33 +13,78 @@ app.use(express.static('public'));
 
 // MONGOOSE CONFIG
 const siteSchema = new mongoose.Schema({
-	long: String,
-	short: String
+	long: {
+		type: String,
+		unique: true
+	},
+	short: {
+		type: String,
+		unique: true
+	}
 });
 
 const Site = mongoose.model('Site', siteSchema);
-
-// Site.create({
-// 	long: 'test',
-// 	short: 'test'
-// });
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/new/:url', (req, res) => {
-	Site.create({
-		long: req.params.url,
-		short: 'test'
+app.get('/new/:url*', async (req, res) => {
+	const fullUrl = req.originalUrl.replace('/new/', '');
+
+	// Check to see if url is valid
+	if (!validUrl.isWebUri(fullUrl)) {
+		res.send({ 'error': 'That is not a valid URL!  Check the format guideline on the homepage.'});
+	}
+	
+	// Look to see if site exists in database
+	await Site.findOne({ 'long': fullUrl}).then(site => {
+		// If the site exists, don't do anything.
+		if (site) {
+			res.send({
+				'Original URL': site.long,
+				'Shortened URL': site.short
+			});
+		}
 	});
-	res.send('hi');
+
+	// If the site doesn't exist, then create an entry
+	const newSite = {
+		long: fullUrl,
+		short: `https://afternoon-tor-86659.herokuapp.com/${shortid.generate()}`
+	};
+
+	Site.create(newSite, (err, res) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(res);
+		}
+	});
+
+	// Show them the newly created object.
+	res.send({
+				'Original URL': newSite.long,
+				'Shortened URL': newSite.short
+	});
+});
+
+app.get('/:url', async (req, res) => {
+	const site = await Site.findOne({ 'short': req.params.url }, (err, res) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(res);
+		}
+	});
+	res.redirect(site.long);
+	
 });
 
 app.listen(port, () => console.log('Server started'));
 
-// User inputs URL
-// Generate random, short string from URL
-// Save short URL and long URL under same document in database
-// Display short URL and long URL to user
-// When someone goes to the short URL from app, redirect to long URL
+// User inputs URL *
+// Generate random, short string from URL *
+// Save short URL and long URL under same document in database *
+// Display short URL and long URL to user 
+// When someone goes to the short URL from app, redirect to long URL *
