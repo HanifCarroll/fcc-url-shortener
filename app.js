c02dfc46e7256582fe53881ad589d3338915c1fa
Hanif Carroll <HCarrollUSF@gmail.com>
2017-07-27 21:52:00 -0400

    Test

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/new/:url*', async (req, res) => {
+app.get('/new/:url', async (req, res) => {
 	const fullUrl = req.originalUrl.replace('/new/', '');
 
 	// Check to see if url is valid
